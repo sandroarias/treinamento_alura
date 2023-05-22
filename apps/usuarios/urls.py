44486a7d4374ff3ba12505a5d079cb2e5d55b82a
sandroarias <sandro901217@gmail.com>
2023-05-22 14:15:04 -0300

    Projeto finalizado

---
+++ apps/usuarios/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from apps.usuarios.views import cadastro, login, logout
+
+urlpatterns = [
+    path('cadastro', cadastro, name='cadastro'),
+    path('login', login, name='login'),
+    path('logout', logout, name='logout'),
+]
